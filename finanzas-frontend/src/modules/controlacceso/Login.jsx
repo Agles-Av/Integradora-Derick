64929cd5188f62f4faa5b7662e34ad106437246a
Agles <20223tn005@utez.edu.mx>
2025-04-16 15:05:13 -0600

    [ADD] Validacion completa de campos

--- finanzas-frontend/src/modules/controlacceso/Login.jsx
+++ finanzas-frontend/src/modules/controlacceso/Login.jsx
@@ -1,4 +1,4 @@
-import React, { use, useState } from 'react';
+import React, { useState } from 'react';
 import { Card } from 'primereact/card';
 import { FloatLabel } from 'primereact/floatlabel';
 import { InputText } from 'primereact/inputtext';
@@ -16,11 +16,23 @@ function Login() {
     const [password, setPassword] = useState('');
     const [visible, setVisible] = useState(false);
     const [email, setEmail] = useState('');
+    const [errors, setErrors] = useState({});
     const { dispatch } = React.useContext(AuthContext);
     const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        // Validaciones
+        const newErrors = {};
+        const usernameValid = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(username);
+        if (!usernameValid) newErrors.username = 'El correo electrónico no es válido';
+        if (!username.trim()) newErrors.username = 'El usuario es requerido';
+        if (!password.trim()) newErrors.password = 'La contraseña es requerida';
+        if (Object.keys(newErrors).length > 0) {
+            setErrors(newErrors);
+            return;
+        }
+        setErrors({});
         try {
             const response = await login(username, password);
             dispatch({
@@ -43,7 +55,6 @@ function Login() {
             setVisible(false);
         } catch (error) {
         }finally{
-            setEmail('');
             setEmail('');
             setVisible(false);
         }
@@ -84,12 +95,24 @@ function Login() {
                                     id="username"
                                     className="w-full"
                                     value={username}
-                                    onChange={(e) => setUsername(e.target.value)}
+                                    onChange={(e) => {
+                                        const value = e.target.value.trim();
+                                        setUsername(value);
+                                        setErrors(prev =>{
+                                            const newErrors = {...prev};
+                                            if(value && /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value)){
+                                                delete newErrors.username;
+                                            }
+                                            return newErrors;
+                                        });
+                                    }}
+                                    maxLength={76}
                                 />
                                 <label htmlFor="username">
                                     <i className="pi pi-user mr-2"></i> Usuario
                                 </label>
                             </FloatLabel>
+                            {errors.username && <small className="p-error">{errors.username}</small>}
                         </div>
 
                         <div className='mt-4'>
@@ -99,12 +122,26 @@ function Login() {
                                     className="w-full"
                                     type="password"
                                     value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
+                                    onChange={(e) => {
+                                        const value = e.target.value.trim();
+                                        setPassword(value);
+                                    
+                                        setErrors(prev => {
+                                            const newErrors = { ...prev };
+                                            if (value) {
+                                                delete newErrors.password;
+                                            }
+                                            return newErrors;
+                                        });
+                                    }}
+                                    maxLength={76}
+                                    
                                 />
                                 <label htmlFor="password">
                                     <i className="pi pi-lock mr-2"></i> Contraseña
                                 </label>
                             </FloatLabel>
+                            {errors.password && <small className="p-error">{errors.password}</small>}
                         </div>
 
                         <div className='mt-4 h-full w-full'>
@@ -112,11 +149,11 @@ function Login() {
                                 label='Iniciar sesión'
                                 type='submit'
                                 className='w-full'
-                                disabled={!username || !password}
+                                disabled={!username || !password || Object.keys(errors).length > 0}
                             />
                         </div>
                     </form>
-                    <div className='felx space-between mt-4'>
+                    <div className=' mt-4'>
                         <p className="text-center">
                             <a href="/register" >
                                 ¿No tienes cuenta? crea una
