362e97d67bf47b30e6e6f4b9ab32fb77e1ebfa5a
Agles <20223tn005@utez.edu.mx>
2025-04-21 10:17:18 -0600

    Proyecto completo

--- finanzas-frontend/vite.config.js
+++ finanzas-frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    allowedHosts:[
+      'f625-187-226-122-129.ngrok-free.app'
+    ]
+  }
 })
